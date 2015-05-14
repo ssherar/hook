1ec5c73584f1dade9a13cd57176095be8f976a90
Sam Sherar <sbsherar@gmail.com>
2015-05-14 21:58:52 +0100

    Take method from payload header instead of URI

--- hook/receiver.py
+++ hook/receiver.py
@@ -1,24 +1,30 @@
-from flask import Flask
+from flask import Flask, request
 from .parser import get_rules
-import logging
 
 app = Flask(__name__)
 rules = get_rules()
+
 
 @app.route("/")
 def home():
     return "Hello world"
 
-@app.route("/hook/<name>/<method>")
-def trigger_hook(name, method):
-    ret = None
+
+@app.route("/hook/<name>", methods=["POST"])
+def trigger_hook(name):
+    headers = request.headers
+
+    try:
+        method = headers["X-Github-Event"]
+    except KeyError:
+        return "Method not in payload", 404
+
     try:
         hook = rules[name]
-        ret = hook.execute_hook(method)
+        return hook.execute_hook(method), 200
     except KeyError:
         return "Hook not found", 404
 
-    return ret, 200
 
 if __name__ == "__main__":
     app.run(debug=True)
